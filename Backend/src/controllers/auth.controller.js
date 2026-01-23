602360ed39dd9243f2c0628f6e413e3dbd166988
felni06-tech <felni06@gmail.com>
2026-01-23 11:27:32 +0100

    Adatbázis kapcsolat létesítése és ellenőrzése

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -3,14 +3,25 @@ import { db } from '../config/db'
 
 export const register = async (req, res) => {
     const { name, email, password, phone, profile_type, profile_picture } = req.body;
-}
 
-if (!name || !email || !password || !phone || !profile_type) {
-        return res.status(400).json({ message: "Minden mező kitöltése kötelező!" });
+
+    if (!name || !email || !password || !phone || !profile_type) {
+            return res.status(400).json({ message: "Minden mező kitöltése kötelező!" });
     }
 
-const passwordHash = await bcrypt.hash(password, 10)
+    try {
+        const [existing] = await db.execute('SELECT user_id FROM users WHERE email = ?', [email])
+        if(existing.length > 0) {
+            return res.status(400).json({message: "Ez az email cím már foglalt."})
+        }
 
-await db.execute(
-    `INSERT INTO users (name, email, )`
-)
+        const salt = await bcrypt.genSalt(10)
+        const passwordHash = await bcrypt.hash(password, salt)
+
+        const connection = await db.getConnection()
+
+    } catch (error) {
+
+    }
+
+}
---
+++ Backend/src/server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+
+
+app.listen(3000, () => {
+    console.log("A szerver fut: http://localhost:3000")
+})
