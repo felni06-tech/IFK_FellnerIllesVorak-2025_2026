4392af6ddc698546cc9ba65122cff7db1520b1d3
felni06-tech <felni06@gmail.com>
2026-01-28 10:59:09 +0100

    user.model javítása az új adatbázishoz

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -9,7 +9,7 @@ const UserModel = {
 
     //Új felhasználó létrehozása
     create: async (userData) => {
-        const { name, email, phone, password_hash, profile_type, profile_picture, profession } = userData
+        const { provider_id, name, email, phone, password_hash, profile_picture } = userData
 
         const connection = await db.getConnection()
         await connection.beginTransaction()
@@ -17,18 +17,18 @@ const UserModel = {
         try {
             //Mentés az users táblába
             const [result] = await connection.execute(
-                `INSERT INTO users (profile_type, profile_picture, name, email, phone, password_hash)
+                `INSERT INTO users ( provider_id, name, email, phone, password_hash, profile_picture)
                 VALUES (?, ?, ?, ?, ?, ?)`,
-                [profile_type, profile_picture || null, name, email, phone, password_hash]
+                [provider_id || null, name, email, phone, password_hash, profile_picture]
             )
 
             const userId = result.insertId
 
             //Ha szolgáltatói profil akkor oda is hozzá kell adnunk
-            if (profile_type === 'provider') {
+            if (provider_id != null) {
                 await connection.execute(
-                    `INSERT INTO providers (user_id, profession, avg_rating)
-                    VALUES (?, ?, ?)`,
+                    `INSERT INTO providers (user_id, avg_rating)
+                    VALUES (?, ?)`,
                     [userId, profession, 0.0]
                 )
             }
